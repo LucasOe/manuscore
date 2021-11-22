7856fce460036fc4b971ce2ba15aeb420b2555ae
Lucas Oelker <lucas.oelker@gmx.de>
2021-11-22 18:00:46 +0100

    Load model data dynamically

---
+++ src/app/Model.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Model {
+    float[] vertices;
+    int[] indices;
+
+    public Model(float[] vertices, int[] indices) {
+        this.vertices = vertices;
+        this.indices = indices;
+    }
+}
