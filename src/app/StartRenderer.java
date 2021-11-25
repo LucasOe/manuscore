31971fe152a9a1895c490d2aedec8081e972247e
LucasOe <github@lucasoe.com>
2021-11-25 18:46:04 +0100

    Refactor code

--- src/app/StartRenderer.java
+++ src/app/StartRenderer.java
@@ -19,21 +19,25 @@ public class StartRenderer extends GLCanvas implements GLEventListener {
     static int activeObject;
 
     // Defining shader source code file paths and names
-    final static String shaderPath = ".\\resources\\shaders\\";
-    final static String modelPath = ".\\resources\\models\\";
+    String shaderPath = ".\\resources\\shaders\\";
+    String modelPath = ".\\resources\\models\\";
 
-    final String vertexShaderFileName = "Basic.vert";
-    final String fragmentShaderFileName = "Basic.frag";
+    String vertexShaderFileName = "Basic.vert";
+    String fragmentShaderFileName = "Basic.frag";
 
-    private static Path[] objectPaths = { Paths.get(modelPath + "suzanne.obj"), Paths.get(modelPath + "heart.obj") };
+    // List of obj files
+    Path[] objectPaths = { Paths.get(modelPath + "suzanne.obj"), Paths.get(modelPath + "heart.obj") };
+
+    // contains the geometry of the obj file
+    Model[] objectData = new Model[objectPaths.length];
 
     // Object for loading shaders and creating a shader program
-    private ShaderProgram shaderProgram;
+    ShaderProgram shaderProgram;
 
     // OpenGL buffer names for data allocation and handling on GPU
-    int[] vaoName; // List of names (integer pointers) of vertex array objects
-    int[] vboName; // List of names (integer pointers) of vertex buffer objects
-    int[] iboName; // List of names (integer pointers) of index buffer objects
+    int[] vaoName;
+    int[] vboName;
+    int[] iboName;
 
     // Declaration of an object for handling keyboard and mouse interactions
     InteractionHandler interactionHandler;
@@ -41,29 +45,18 @@ public class StartRenderer extends GLCanvas implements GLEventListener {
     // Declaration for using the projection-model-view matrix tool
     PMVMatrix pmvMatrix;
 
-    // contains the geometry of our OBJ file
-    static Model[] objectData = new Model[objectPaths.length];
+    GL3 gl;
 
     public StartRenderer() {
         // Create the OpenGL canvas with default capabilities
         super();
+
         // Add this object as OpenGL event listener to the canvas
         this.addGLEventListener(this);
         createAndRegisterInteractionHandler();
     }
 
-    public StartRenderer(GLCapabilities capabilities) {
-        // Create the OpenGL canvas with the requested OpenGL capabilities
-        super(capabilities);
-        // Add this object as an OpenGL event listener to the canvas
-        this.addGLEventListener(this);
-        createAndRegisterInteractionHandler();
-    }
-
     private void createAndRegisterInteractionHandler() {
-        // The constructor call of the interaction handler generates meaningful default values.
-        // The start parameters can also be set via setters
-        // (see class definition of the interaction handler).
         interactionHandler = new InteractionHandler();
         this.addKeyListener(interactionHandler);
         this.addMouseListener(interactionHandler);
@@ -73,11 +66,11 @@ public class StartRenderer extends GLCanvas implements GLEventListener {
 
     @Override
     public void init(GLAutoDrawable drawable) {
-        // Retrieve the OpenGL graphics context
-        GL3 gl = drawable.getGL().getGL3();
+        // Get the OpenGL graphics context
+        gl = drawable.getGL().getGL3();
+
         // Outputs information about the available and chosen profile
         System.err.println("Chosen GLCapabilities: " + drawable.getChosenGLCapabilities());
-        System.err.println("INIT GL IS: " + gl.getClass().getName());
         System.err.println("GL_VENDOR: " + gl.glGetString(GL.GL_VENDOR));
         System.err.println("GL_RENDERER: " + gl.glGetString(GL.GL_RENDERER));
         System.err.println("GL_VERSION: " + gl.glGetString(GL.GL_VERSION));
@@ -85,18 +78,18 @@ public class StartRenderer extends GLCanvas implements GLEventListener {
         // Loading the vertex and fragment shaders and creation of the shader program.
         shaderProgram = new ShaderProgram(gl);
         ShaderProgram.loadShaderAndCreateProgram(shaderPath, vertexShaderFileName, fragmentShaderFileName);
+        // Use the compiled shaderProgram
+        gl.glUseProgram(shaderProgram.getShaderProgramID());
 
         // Create object for projection-model-view matrix calculation.
         pmvMatrix = new PMVMatrix();
 
-        // Vertices for drawing a triangle.
-        // To be transferred to a vertex buffer object on the GPU.
-        // Interleaved data layout: position, color
+        // Create a Model object and load the vertices and indicies from the obj file
         for (int i = 0; i < objectPaths.length; i++) {
             try {
                 Mesh mesh = new OBJLoader().setLoadNormals(true) // tell the loader to also load normal data
                         .setGenerateIndexedMeshes(true) // tell the loader to output separate index arrays
-                        .loadMesh(Resource.file(objectPaths[i])); // actually load the file
+                        .loadMesh(Resource.file(objectPaths[i])); // load the file obj file
 
                 objectData[i] = new Model(mesh.getVertices(), mesh.getIndices());
             } catch (IOException e) {
@@ -104,16 +97,40 @@ public class StartRenderer extends GLCanvas implements GLEventListener {
             }
         }
 
+        // Create the Vertex Array Object on the GPU
+        vaoName = new int[1];
+        gl.glGenVertexArrays(1, vaoName, 0);
+        gl.glBindVertexArray(vaoName[0]);
+
+        // Creating the Vertex Buffer Objects on the GPU
+        vboName = new int[1];
+        gl.glGenBuffers(1, vboName, 0);
+        gl.glBindBuffer(GL.GL_ARRAY_BUFFER, vboName[0]);
+        gl.glBufferData(GL.GL_ARRAY_BUFFER, objectData[activeObject].getVertices().length * Float.BYTES,
+                FloatBuffer.wrap(objectData[activeObject].getVertices()), GL.GL_STATIC_DRAW);
+
+        // Creating the Index Buffer Objects on the GPU
+        iboName = new int[1];
+        gl.glGenBuffers(1, iboName, 0);
+        gl.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, iboName[0]);
+        gl.glBufferData(GL.GL_ELEMENT_ARRAY_BUFFER, objectData[activeObject].getIndices().length * Integer.BYTES,
+                IntBuffer.wrap(objectData[activeObject].getIndices()), GL.GL_STATIC_DRAW);
+
         // Enable alpha transparency
         gl.glEnable(GL.GL_BLEND);
         gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
 
+        // Enable layout position 0
+        gl.glEnableVertexAttribArray(0);
+        // Map layout position 0 to the position information per vertex in the VBO.
+        gl.glVertexAttribPointer(0, 3, GL.GL_FLOAT, false, 6 * Float.BYTES, 0);
+        // Enable layout position 1
+        gl.glEnableVertexAttribArray(1);
+        // Map layout position 1 to the color information per vertex in the VBO.
+        gl.glVertexAttribPointer(1, 3, GL.GL_FLOAT, false, 6 * Float.BYTES, 3 * Float.BYTES);
+
         // Set start parameter(s) for the interaction handler.
         interactionHandler.setEyeZ(2);
-
-        // Switch on back face culling
-        // gl.glEnable(GL.GL_CULL_FACE);
-        //gl.glCullFace(GL.GL_BACK);
 
         // Switch on depth test.
         gl.glEnable(GL.GL_DEPTH_TEST);
@@ -124,18 +141,8 @@ public class StartRenderer extends GLCanvas implements GLEventListener {
 
     @Override
     public void display(GLAutoDrawable drawable) {
-        // Retrieve the OpenGL graphics context
-        GL3 gl = drawable.getGL().getGL3();
         // Clear color and depth buffer
         gl.glClear(GL3.GL_COLOR_BUFFER_BIT | GL3.GL_DEPTH_BUFFER_BIT);
-
-        // Controlling the interaction settings
-        /*        System.out.println("Camera: z = " + interactionHandler.getEyeZ() + ", " +
-                "x-Rot: " + interactionHandler.getAngleXaxis() +
-                ", y-Rot: " + interactionHandler.getAngleYaxis() +
-                ", x-Translation: " + interactionHandler.getxPosition()+
-                ", y-Translation: " + interactionHandler.getyPosition());// definition of translation of model (Model/Object Coordinates --> World Coordinates)
-        */
 
         // Apply view transform using the PMV-Tool
         // Camera positioning is steered by the interaction handler
@@ -145,64 +152,11 @@ public class StartRenderer extends GLCanvas implements GLEventListener {
         pmvMatrix.glRotatef(interactionHandler.getAngleXaxis(), 1f, 0f, 0f);
         pmvMatrix.glRotatef(interactionHandler.getAngleYaxis(), 0f, 1f, 0f);
 
-        /*
-            Begin
-        */
-
-        // Create and activate a vertex array object (VAO)
-        // Useful for switching between data sets for object rendering.
-        vaoName = new int[1];
-        // Creating the buffer on GPU.
-        gl.glGenVertexArrays(1, vaoName, 0);
-        if (vaoName[0] < 1)
-            System.err.println("Error allocating vertex array object (VAO) on GPU.");
-        // Switch to this VAO.
-        gl.glBindVertexArray(vaoName[0]);
-
-        // Create, activate and initialize vertex buffer object (VBO)
-        // Used to store vertex data on the GPU.
-        vboName = new int[1];
-        // Creating the buffer on GPU.
-        gl.glGenBuffers(1, vboName, 0);
-        if (vboName[0] < 1)
-            System.err.println("Error allocating vertex buffer object (VBO) on GPU.");
-        // Activating this buffer as vertex buffer object.
-        gl.glBindBuffer(GL.GL_ARRAY_BUFFER, vboName[0]);
-        // Transferring the vertex data (see above) to the VBO on GPU.
-        // (floats use 4 bytes in Java)
-        gl.glBufferData(GL.GL_ARRAY_BUFFER, objectData[activeObject].vertices.length * Float.BYTES,
-                FloatBuffer.wrap(objectData[activeObject].vertices), GL.GL_STATIC_DRAW);
-
-        iboName = new int[1];
-        // Creating the buffer on GPU.
-        gl.glGenBuffers(1, iboName, 0);
-        if (iboName[0] < 1)
-            System.err.println("Error allocating vertex buffer object (VBO) on GPU.");
-
-        gl.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, iboName[0]);
-        gl.glBufferData(GL.GL_ELEMENT_ARRAY_BUFFER, objectData[activeObject].indices.length * Integer.BYTES,
-                IntBuffer.wrap(objectData[activeObject].indices), GL.GL_STATIC_DRAW);
-
-        // Activate and map input for the vertex shader from VBO,
-        // taking care of interleaved layout of vertex data (position and color),
-        // Enable layout position 0
-        gl.glEnableVertexAttribArray(0);
-        // Map layout position 0 to the position information per vertex in the VBO.
-        gl.glVertexAttribPointer(0, 3, GL.GL_FLOAT, false, 6 * Float.BYTES, 0);
-        // Enable layout position 1
-        gl.glEnableVertexAttribArray(1);
-        // Map layout position 1 to the color information per vertex in the VBO.
-        gl.glVertexAttribPointer(1, 3, GL.GL_FLOAT, false, 6 * Float.BYTES, 3 * Float.BYTES);
-
-        /*
-            End
-        */
-
-        // Switch to this vertex buffer array for drawing.
-        gl.glBindVertexArray(vaoName[0]);
-        // Activating the compiled shader program.
-        // Could be placed into the init-method for this simple example.
-        gl.glUseProgram(shaderProgram.getShaderProgramID());
+        // Create the BufferData every single frame to the current active object
+        gl.glBufferData(GL.GL_ARRAY_BUFFER, objectData[activeObject].getVertices().length * Float.BYTES,
+                FloatBuffer.wrap(objectData[activeObject].getVertices()), GL.GL_STATIC_DRAW);
+        gl.glBufferData(GL.GL_ELEMENT_ARRAY_BUFFER, objectData[activeObject].getIndices().length * Integer.BYTES,
+                IntBuffer.wrap(objectData[activeObject].getIndices()), GL.GL_STATIC_DRAW);
 
         // Transfer the PVM-Matrix (model-view and projection matrix) to the GPU
         // via uniforms
@@ -212,20 +166,14 @@ public class StartRenderer extends GLCanvas implements GLEventListener {
         gl.glUniformMatrix4fv(1, 1, false, pmvMatrix.glGetMvMatrixf());
 
         // Draw the triangles using the indices array
-        gl.glDrawElements(GL.GL_TRIANGLES, // mode
-                objectData[activeObject].indices.length, // count
-                GL.GL_UNSIGNED_INT, // type
-                0 // element array buffer offset
-        );
+        gl.glDrawElements(GL.GL_TRIANGLES, objectData[activeObject].indices.length, GL.GL_UNSIGNED_INT, 0);
     }
 
     @Override
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-        // Retrieve the OpenGL graphics context
-        GL3 gl = drawable.getGL().getGL3();
-
-        // Set the viewport to the entire window
+        // Set the viewport width and height to the entire window
         gl.glViewport(0, 0, width, height);
+
         // Switch the pmv-tool to perspective projection
         pmvMatrix.glMatrixMode(PMVMatrix.GL_PROJECTION);
         // Reset projection matrix to identity
@@ -239,12 +187,9 @@ public class StartRenderer extends GLCanvas implements GLEventListener {
         pmvMatrix.glMatrixMode(PMVMatrix.GL_MODELVIEW);
     }
 
+    // Called by the drawable before the OpenGL context is destroyed by an external event
     @Override
     public void dispose(GLAutoDrawable drawable) {
-        // Retrieve the OpenGL graphics context
-        GL3 gl = drawable.getGL().getGL3();
-        System.out.println("Deleting allocated objects, incl. the shader program.");
-
         // Detach and delete shader program
         gl.glUseProgram(0);
         shaderProgram.deleteShaderProgram();
@@ -259,7 +204,9 @@ public class StartRenderer extends GLCanvas implements GLEventListener {
         System.exit(0);
     }
 
-    public static void clickButton(int active) {
+    // Called by the EvenHandler in MainWindow.java
+    public void clickButton(int active) {
         activeObject = active;
+        System.out.println("Loading: " + objectPaths[activeObject]);
     }
 }
