12891bd2fbc3becabe54fd63f67a6242a23a7425
LucasOe <lucas.oelker@gmx.de>
2021-12-23 13:41:49 +0100

    Add JavaDoc comments to Utils

--- src/app/Utils.java
+++ src/app/Utils.java
@@ -14,8 +14,15 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.imgcodecs.Imgcodecs;
 
+/**
+ * Klasse mit statischen Helfermethoden.
+ */
 public class Utils {
-	// Read image from file and return the buffered image
+	/**
+	 * Gibt ein Bild aus einem Dateipfad als BufferedImage zurück.
+	 * @param path	Dateipfad zum Bild
+	 * @return		BufferedImage
+	 */
 	public static BufferedImage readImageFile(String path) {
 		try {
 			return ImageIO.read(new File(path));
@@ -25,11 +32,22 @@ public class Utils {
 		}
 	}
 
-	// Resize image to set width and height
+	/**
+	 * Skaliert ein Bild auf die anegegebene Breite und Höhe.
+	 * @param image		Eingabebild
+	 * @param width		Breite
+	 * @param height	Höhe
+	 * @return			Ausgabebild
+	 */
 	public static Image resizeImage(Image image, int width, int height) {
 		return image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
 	}
 
+	/**
+	 * Speichert ein Bild mit dem angegebenen Pfad als jpg.
+	 * @param image			Bild das gespeichert werden soll
+	 * @param outputPath	Pfad
+	 */
 	public static void writeFile(Image image, String outputPath) {
 		try {
 			File outputfile = new File(outputPath + ".jpg");
@@ -40,6 +58,12 @@ public class Utils {
 		}
 	}
 
+	/**
+	 * Konvertiert ein BufferedImage in eine Mat.
+	 * @param image		Eingabebild
+	 * @return			Ausgabematrix
+	 * @throws IOException
+	 */
 	public static Mat BufferedImage2Mat(BufferedImage image) throws IOException {
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 		ImageIO.write(image, "jpg", byteArrayOutputStream);
@@ -47,6 +71,12 @@ public class Utils {
 		return Imgcodecs.imdecode(new MatOfByte(byteArrayOutputStream.toByteArray()), Imgcodecs.IMREAD_UNCHANGED);
 	}
 
+	/**
+	 * Konvertiert eine Mat in ein BufferedImage.
+	 * @param matrix	Eingabematrix
+	 * @return			Ausgabebild
+	 * @throws IOException
+	 */
 	public static BufferedImage Mat2BufferedImage(Mat matrix) throws IOException {
 		MatOfByte mob = new MatOfByte();
 		Imgcodecs.imencode(".jpg", matrix, mob);
